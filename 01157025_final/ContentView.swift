06e81bd8eae029cb5a146479df86c02f73ca206b
wonogfsocry <yangneil930406@gmail.com>
2024-11-28 09:29:06 +0800

    Initial Commit

---
+++ 01157025_final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01157025_final
+//
+//  Created by user10 on 2024/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01157025_final/_1157025_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1157025_finalApp.swift
+//  01157025_final
+//
+//  Created by user10 on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct _1157025_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
